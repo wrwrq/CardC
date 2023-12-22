2ae390458034d095b8befbd09562dfafbad3146f
hygge31 <yiuter1@hanmail.net>
2023-12-22 16:30:44 +0900

    게임 매니저
    게임 매니저, 보드판 생성

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Transform cardHolder;
+
+
+
+    [Header("State")]
+    public float boardSize;
+
+    [Header("Card")]
+    public GameObject cardPrefeb;
+    public float cardSizeX;
+    public float cardSizeY;
+    [Range(0,1)]
+    public float outLinePercent;
+
+    [Space(10)]
+    public List<Sprite> cardImages = new List<Sprite>();
+
+    private void Start()
     {
-        
+        GeneratorBoard();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void GeneratorBoard()
     {
-        
+      for(int i = 0; i< boardSize; i++)
+        {
+            for(int j = 0; j< boardSize; j++)
+            {
+                float x = (-boardSize / 2 + i + 0.5f) * cardSizeX;
+                float y = (-boardSize / 2 + j + 0.5f) * cardSizeY;
+                GameObject newCard = Instantiate(cardPrefeb, new Vector3(x, y, 0), Quaternion.identity);
+                newCard.transform.localScale = newCard.transform.localScale * (1 - outLinePercent);
+
+                newCard.transform.SetParent(cardHolder);
+
+            }
+        }  
     }
 }
