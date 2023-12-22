fc4e526f8108dc50eed3a7b0cc43ad8f34722e94
jihye <thoth_0302@naver.com>
2023-12-22 16:33:56 +0900

    소개글 cs 추가 - 이미지 이름 맞게 변경 필요

---
+++ Assets/Scripts/Introduction.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introduction : MonoBehaviour
+{
+    string n;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void matchName(string name)
+    {
+        transform.Find(name).gameObject.SetActive(true);
+        n = name;
+    }
+
+    void nonmatchName(string name)
+    {
+        transform.Find(name).gameObject.SetActive(false);
+    }
+    public void ClickName()
+    {
+        gameObject.SetActive(false);
+        nonmatchName(n);
+        Time.timeScale = 1f;
+    }
+}
