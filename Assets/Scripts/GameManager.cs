f828bcc07d386708b3539ac0cc1123acace3aaa2
hygge31 <yiuter1@hanmail.net>
2023-12-22 16:58:15 +0900

    카드 섞기 함수 수정
    카드 섞기 함수 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,32 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Game State")]
+    public int boardSize;
 
-    void Mix(int[] mix)
-    {//               ¹«ÀÛÀ§·Î ¼¯´Â ÇÔ¼öÀÔ´Ï´Ù. int¹è¿­À» ³Ö¾îÁÖ¼¼¿ä
-        System.Random ran = new System.Random();
-        int temp;
-        int randomIndex;
-        for (int i = 0; i < mix.Length; i++)
+    [Header("Card")]
+    int[] prefebIdxs;
+
+    public List<Sprite> cardImages = new List<Sprite>();
+    Queue<int> queue = new Queue<int>();
+
+    //void Mix(int[] mix)
+    //{//               ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½Ô¼ï¿½ï¿½Ô´Ï´ï¿½. intï¿½è¿­ï¿½ï¿½ ï¿½Ö¾ï¿½ï¿½Ö¼ï¿½ï¿½ï¿½
+    //    System.Random ran = new System.Random();
+    //    int temp;
+    //    int randomIndex;
+    //    for (int i = 0; i < mix.Length; i++)
+    //    {
+    //        randomIndex = ran.Next(0, mix.Length);
+    //        temp = mix[i];
+    //        mix[i] = mix[randomIndex];
+    //        mix[randomIndex] = temp;
+    //    }
+    //}
+    void Shuffle()
+    {
+        for (int i = 0; i < boardSize * boardSize; i++)
         {
-            randomIndex = ran.Next(0, mix.Length);
-            temp = mix[i];
-            mix[i] = mix[randomIndex];
-            mix[randomIndex] = temp;
+            prefebIdxs[i] = (i / 2) % cardImages.Count;
         }
+
+        for (int i = 0; i < boardSize * boardSize; i++)
+        {
+            int randomIdx = Random.Range(i, boardSize * boardSize);
+            int term = prefebIdxs[randomIdx];
+            prefebIdxs[randomIdx] = prefebIdxs[i];
+            prefebIdxs[i] = term;
+        }
+
+        queue = new Queue<int>(prefebIdxs);
+
     }
 }
